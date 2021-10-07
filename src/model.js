a38e409818e518f5b66062a3e8416e371b4c319b
TakumiFuji <takumifujiwaraa@mail.ru>
2021-10-07 20:15:58 +0200

    first commit x2

--- src/model.js
+++ src/model.js
@@ -1,14 +1,9 @@
-const model = [
+export const model = [
   { type: "title", value: "Hello World from JS" },
   { type: "text", value: "here we go with some text" },
   {
     type: "columns",
     value: ["111111111111111", "222222222222222", "333333333333333"],
   },
+  { type: "image", value: "./assets/image.jpg" },
 ]
-
-const $site = document.querySelector("#site")
-
-model.forEach((block) => {
-  console.log(block)
-})
